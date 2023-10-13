35c7836ece2587b335eb7a9aa3e56ff4ff9de9a4
Nasrin Raha <59247592+Nasrin-Raha@users.noreply.github.com>
2023-10-13 11:36:50 +0200

    Add files via upload

---
+++ geometry.py
@@ -0,0 +1,17 @@
+import math
+
+def area_of_rectangle(length, breadth):
+    """
+    This function returns the area of a rectangle
+    """
+    area = length * breadth
+    return area
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle
+    """
+    area = math.pi * (radius ** 2)
+    return area
+
+
